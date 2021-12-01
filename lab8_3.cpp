40f47137600736e473297844d4990034618c88d7
mon6996 <thanaporn_chan@cmu.ac.th>
2021-12-01 18:14:00 +0700

    8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	if(x > 'A' && x <= 'Z')
+	{
+		return x - 1;
+	}
+	if(x == 'A')
+	{
+		return 'Z';
+	}
+	else
+	{
+		return '0';
+	}
 }
 
-int main(){
-	//Test Case
+int main()
+{
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
